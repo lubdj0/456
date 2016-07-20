84e05ef54db45785255452aae140646c0fd0c30f
lubdj0 <kone5561@gmail.com>
2016-07-20 16:03:33 +0800

    fuckyou

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public GameObject prefab001;
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Instantiate (prefab001);
+		}
+	
+	}
+}
